c0844911077da30d2c63cc09641cd631feee6e98
fzajic <filip.zajic007@gmail.com>
2019-11-24 21:12:44 +0100

    type-speed tester, my own type-speed tester

--- Linked_In_learning/Ex_Files_JavaScript_EssT/Exercise Files/06_02/script.js
+++ Linked_In_learning/Ex_Files_JavaScript_EssT/Exercise Files/06_02/script.js
@@ -1,3 +1,27 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPostition = (hr*360/12)+((min*360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/50)
+let secPosition = sec*360/60;
+
+function runTheClock (){
+
+  hrPostition = hrPostition + (3/36)
+  secPosition = secPosition + 6;
+  minPosition = minPosition + 6/60
+
+  HOURHAND.style.transform = "rotate(" + hrPostition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+
+var interval = setInterval(runTheClock, 1000);
--- Linked_In_learning/Ex_Files_JavaScript_EssT/Exercise Files/07_03/JS/script.js
+++ Linked_In_learning/Ex_Files_JavaScript_EssT/Exercise Files/07_03/JS/script.js
@@ -1,1 +1,13 @@
+window.onload = function () {
+  const CTA = document.querySelector(".cta a");
+  const ALERT = document.querySelector("#booking-alert")
 
+  CTA.classlist.remove("hide");
+  ALERT.classlist.add("hide");
+
+  function reveal() {
+    CTA.classlist.toggle("hide");
+    ALERT.classlist.toggle("hide");
+  }
+  CTA.onclick = reveal;
+}
