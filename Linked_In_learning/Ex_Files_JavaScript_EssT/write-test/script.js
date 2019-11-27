482ae83b124cde65b2c0015d23cae52ff4dbeb7b
fzajic <filip.zajic007@gmail.com>
2019-11-27 06:44:10 +0100

    Changes to speed-test project

---
+++ Linked_In_learning/Ex_Files_JavaScript_EssT/write-test/script.js
@@ -0,0 +1,5 @@
+const time = document.querySelector("#timer.stat");
+const accuracy = document.querySelector("#accuracy.stat");
+const speed = document.querySelector("#speed.stat");
+
+console.log("HELLO");
